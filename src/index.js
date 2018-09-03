3928c074c934700124b991f0be727c2ab1542ac2
inam95 <mfihassan99@gmail.com>
2018-09-03 20:57:50 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+
+import Routes from './routes';
+import './Resources/css/app.css';
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
